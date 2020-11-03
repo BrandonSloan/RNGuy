72a0586851e91d0a3ff50fd7c24c6938b25291ef
Blake-Henderson <bmhenderson101@gmail.com>
2020-11-03 15:02:56 -0600

    broken animation for prof. to look at

--- Assets/Scripts/Enemy_Controler.cs
+++ Assets/Scripts/Enemy_Controler.cs
@@ -1,26 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+//Author: Blake Henderson
+//Date : 11/3/2020
 
 public class Enemy_Controler : MonoBehaviour
 {
+    /// <summary>
+    /// The health of the enemy
+    /// </summary>
     public float enemy_Health = 30f;
+    /// <summary>
+    /// The actual game object of the enemy
+    /// </summary>
     public GameObject enemy;
+    /// <summary>
+    /// Tells if the enemy can shoot
+    /// </summary>
+    public bool can_Shoot = false;
+    /// <summary>
+    /// The time in between enemy shots
+    /// </summary>
+    public float shot_delay = 1.5f;
+    /// <summary>
+    /// The projectile the enemy shoots
+    /// </summary>
+    public GameObject shot;
+    /// <summary>
+    /// Where the projectiles spawn
+    /// </summary>
+    public GameObject spawn;
+    /// <summary>
+    /// The player game object
+    /// </summary>
     private GameObject player;
+    /// <summary>
+    /// the play script attached to the player
+    /// </summary>
     private Player_Controler player_Script;
+    /// <summary>
+    /// Used to time in between shots
+    /// </summary>
+    private float timer = 0.0f;
+
+    // Start is called when the enemy is spawned
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         player_Script = player.GetComponent<Player_Controler>();
     }
+    // Update is called once per frame
     void Update()
     {
-        if(enemy_Health <= 0)
+        timer += Time.deltaTime;
+        if(timer >= shot_delay)
+        {
+            timer = 0.0f;
+            if (can_Shoot)
+            {
+                shootAtPlayer();
+            }
+        }
+        if (enemy_Health <= 0)
         {
             Destroy(enemy);
         }
     }
-
+    /// <summary>
+    /// Happens when the enemy hits another object
+    /// </summary>
+    /// <param name="collision">The object the enemy bumps into</param>
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player_Bullet")
@@ -29,5 +79,11 @@ public class Enemy_Controler : MonoBehaviour
             enemy_Health -= player_Script.getBaseDamage() * bullet_Damage;
             Destroy(collision.gameObject);
         }
+    }
+    void shootAtPlayer()
+    {
+        float angle = Mathf.Atan2(player.transform.position.y - spawn.transform.position.y,
+            player.transform.position.x - spawn.transform.position.x) * Mathf.Rad2Deg;
+        Instantiate(shot, spawn.transform.position, Quaternion.Euler(0, 0, angle - 90));
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Author: Blake Henderson
+//Date : 10/23/2020
+public class Projectile : MonoBehaviour
+{
+    /// <summary>
+    /// The force behind the bullet
+    /// </summary>
+    public int thrust = 100;
+    /// <summary>
+    /// The rigid body of the projectile
+    /// </summary>
+    private Rigidbody2D rb2d;
+    /// <summary>
+    /// The game manager
+    /// </summary>
+    //private GameManager gameManager;
+    //private EnemyHealth enemyHealth;
+    //private enviromentHealth envinroHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+        rb2d = GetComponent<Rigidbody2D>();
+        BulletDepawn();
+        transform.rotation.Set(0f, 0f, 90f, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb2d.AddForce(transform.up * thrust);
+    }
+    /// <summary>
+    /// Despawns the bullet after a given number of time.
+    /// </summary>
+    private void BulletDepawn()
+    {
+        Destroy(gameObject, 0.75f);
+    }
+}
